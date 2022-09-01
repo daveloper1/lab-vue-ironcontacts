f3410e607378728bc96440f156450797c239042a
David Leon <97davidleon@gmail.com>
2022-09-01 21:06:37 +0200

    Iteration 1 done

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,48 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <table>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+      <tr v-for="contact in getContacts" :key="contact.id">
+        <td>
+          <img
+            class="contact-image"
+            :src="`${contact.pictureUrl}`"
+            alt="Contact Image"
+          />
+        </td>
+        <td>{{ contact.name }}</td>
+        <td>{{ contact.popularity }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
+<script>
+import { computed } from "@vue/runtime-core";
+import contactsData from "../src/contacts.json";
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      contacts: contactsData,
+    };
+  },
+  computed: {
+    getContacts() {
+      const nElements = 5;
+      return this.contacts.slice(0, nElements);
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.contact-image {
+  height: 100px;
 }
 </style>
