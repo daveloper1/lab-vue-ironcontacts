df7809ab8e423988e617ce1f6e0fca8ab29f3002
David Leon <97davidleon@gmail.com>
2022-09-03 13:59:30 +0200

    Added forgotten modification

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
   <div>
     <NavBar />
-    <ContactsList/>
+    <ContactsList />
   </div>
 </template>
 
 <script>
 import { computed } from "@vue/runtime-core";
-import contactsData from "../src/contacts.json";
 import ContactsList from "./components/ContactsList.vue";
 import NavBar from "./components/NavBar.vue";
 export default {
   name: "App",
   components: { ContactsList: ContactsList, NavBar: NavBar },
-  
 };
 </script>
 
 <style>
-.contact-image {
-  height: 100px;
-}
+
 </style>
